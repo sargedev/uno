52d50863f3edaaa1d818215c3116bfda4cd0b01a
Sarge <63020240+sargedev@users.noreply.github.com>
2024-06-12 21:04:54 +0200

    Added Card class and card property enums

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
 
+enum CardType {
+    Colored,
+    Wild
+}
+
+enum CardColor {
+    Red,
+    Blue,
+    Yellow,
+    Green
+}
+
+class Card {
+    type: CardType;
+    color?: CardColor;
+    value?: number;
+
+    constructor(type: CardType, color?: CardColor, value?: number) {
+        this.type = type;
+        this.color = color;
+        this.value = value;
+    }
+}
